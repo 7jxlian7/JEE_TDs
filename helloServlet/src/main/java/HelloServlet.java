f5b87026eb8a073d92ad985d88d46952bdcfd144
jforme <julian.forme@etu.u-bordeaux.fr>
2022-03-04 14:31:37 +0100

    Iajout servlet

---
+++ helloServlet/src/main/java/HelloServlet.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println( "<HTML>" );
+        out.println( "<HEAD>");
+        out.println( "<TITLE>Mon super site !</TITLE>" );
+        out.println( "</HEAD>" );
+        out.println( "<BODY>" );
+        out.println( "<H1>Hello !</H1>" );
+        out.println( "<p>Bienvenue sur ma <span class=\"red\">page</span> !</p>" );
+        out.println( "<style>" +
+                    ".red { color: red; }" +
+                    "</style>" );
+        out.println("<img src=\"https://www.zooplus.fr/magazine/wp-content/uploads/2019/06/arriv%C3%A9e-dun-chaton-%C3%A0-la-maison.jpeg\" alt=\"Image de petit chatoooon\" />");
+        out.println( "</BODY>" );
+        out.println( "</HTML>" );
+        out.close();
+    }
+}
